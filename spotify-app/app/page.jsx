7a8deede32863b1b6e3e4b2c4c12595dd41c6fd1
Nicolodean <s10262596@connect.np.edu.sg>
2024-01-15 22:02:52 +0800

    testing push

---
+++ spotify-app/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+
+
+export default function Home() {
+    return (<p>Hello</p>)
+}
+
+export default function Test() {
+    return (<p>Bryan smells like fish</p>)
+}
