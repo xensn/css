55b5de51a422b45f4e2d5821f410f07501aaf38e
xavierskippy <s10258417@connect.np.edu.sg>
2024-01-15 23:54:52 +0800

    added some rows

--- spotify-app/app/page.jsx
+++ spotify-app/app/page.jsx
@@ -1,16 +1,29 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
-export default function Home() {
-    return (<p>Hello</p>)
-}
-
-export default function Test() {
-    return (<p>Bryan smells like fish</p>)
-}
-
-export default function Hello(){
-    return <p>Hello</p>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container,InputGroup,FormControl,Button,Row,Card } from 'react-bootstrap'
+import { useState,useEffect } from 'react'
+function App() {
+    const [SearchInput,setSearchInput] = useState("");
+    return(
+        <div className="App">
+            <Container>
+                <InputGroup className="mb-3" size="lg">
+                    <FormControl
+                        placeholder= "Search For Artist"
+                        type = "input"
+                        onKeyPress={event => {
+                            if(event.key == "Enter"){
+                                console.log("Pressed enter");
+                            }
+                        }}
+                        onChange = {event => setSearchInput(event.target.value)}
+                    />
+                    
+                </InputGroup>
+            </Container>
+        </div>
+    );
 }
 export default function Eason(){
     return <p>Hey</p>
